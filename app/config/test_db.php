d24f3c8d0a2e96cbd3c31146b19a598ddacbdbb0
ihor.krasnov <igorkrasnov01@gmail.com>
2024-12-01 14:46:08 +0200

    Configure codeceptionb unit tests

--- app/config/test_db.php
+++ app/config/test_db.php
@@ -3,4 +3,10 @@ $db = require __DIR__ . '/db.php';
 // test database! Important not to run tests on production or development databases
 $db['dsn'] = 'mysql:host=localhost;dbname=yii2basic_test';
 
-return $db;
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'pgsql:host=localhost;port=5432;dbname=musciblogdb',
+    'username' => 'postgres', 
+    'password' => 'postgres',
+    'charset' => 'utf8',
+];
